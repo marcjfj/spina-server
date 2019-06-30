6a74b66cf0421a0bc5ec4dc7ffac8d26126590dc
Marc <marcjfj@gmail.com>
2019-06-30 16:43:10 -0500

    favicon

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 // app.use(express.static('node_modules'));
 
-app.use('/favicon.ico', express.static('images/favicon.ico'));
+// app.use('/favicon.ico', express.static('images/favicon.ico'));
 
 app.use('/', indexRouter);
 app.use('/attractions', attractionsRouter);
